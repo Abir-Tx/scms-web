d2ad6985deff97ff7cb143a6ef6e90ecad65f2f7
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2023-12-06 20:48:44 +0600

    chore: Add homepage bg and welcome text
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import Head from "next/head";
 
 export default function Home() {
-  return <main className={styles.main}>Hello</main>;
+  return (
+    <>
+      <Head>
+        <title>SCMS Web | Home</title>
+      </Head>
+      <main className={styles.main}>
+        <section className={`${styles.firstSection}`}>
+          <div className={styles.bgImageCon}>
+            <div className={styles.heroTextCon}>
+              <h4>Welcome To</h4>
+              <h1>SCMS Website</h1>
+            </div>
+          </div>
+        </section>
+      </main>
+      ;
+    </>
+  );
 }
