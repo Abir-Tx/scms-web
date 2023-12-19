2823ae84aac7e62758faf4b49934144457860e17
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2023-12-19 12:56:52 +0600

    Add animated page and styling to login page
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { AnimatedPage } from "../components/animated-page";
+import styles from "./login.module.scss";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className={styles.loginContainer}>
+      <AnimatedPage>
+        <div className={styles.boxContainer}>
+          <h1>Login</h1>
+          <div className={styles.box}>
+            <form>
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" />
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </div>
+      </AnimatedPage>
+    </div>
+  );
 }
