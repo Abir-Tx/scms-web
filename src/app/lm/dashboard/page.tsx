fdfae50ed00b6102ab034505696c75f57a290848
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2023-12-22 09:43:46 +0600

    [TRY] Create an API call for dashboard

--- src/app/lm/dashboard/page.tsx
+++ src/app/lm/dashboard/page.tsx
@@ -1,6 +1,28 @@
+"use client";
+import api from "@/app/api";
 import styles from "./dashboard.module.scss";
+import { useEffect, useState } from "react";
+
+interface driverData {
+  name: string;
+}
 
 export default function Dashboard() {
+  const [data, setData] = useState<driverData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get<driverData[]>("/drivers/3");
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.dashCon}>
       <div className={styles.quickviewContainer}>
@@ -9,7 +31,7 @@ export default function Dashboard() {
             <span>Drivers</span>
           </div>
           <div className={styles.statConBody}>
-            <span>5</span>
+            <span>{data.name}</span>
           </div>
         </div>
 
