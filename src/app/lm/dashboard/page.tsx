b6e0781a88497bd936ac3261e3fba8989c6acbd4
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-12-24 17:33:37 +0600

    Add shipment data to Dashboard component
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- src/app/lm/dashboard/page.tsx
+++ src/app/lm/dashboard/page.tsx
@@ -11,17 +11,24 @@ interface vehicleData {
   status: string;
 }
 
+interface shipmentData {
+  status: string;
+}
+
 export default function Dashboard() {
   const [driverData, setData] = useState<driverData[]>([]);
   const [vehicleData, setVehicleData] = useState<vehicleData[]>([]);
+  const [shipmentData, setShipmentData] = useState<shipmentData[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await api.get<driverData[]>("/drivers");
         const vehicleResponse = await api.get<vehicleData[]>("/transports");
+        const shipmentResponse = await api.get<shipmentData[]>("/shipments");
         setData(response.data);
         setVehicleData(vehicleResponse.data);
+        setShipmentData(shipmentResponse.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -48,6 +55,15 @@ export default function Dashboard() {
           </div>
           <div className={styles.statConBody}>
             <span>{vehicleData.length}</span>
+          </div>
+        </div>
+
+        <div className={styles.statCon}>
+          <div className={styles.statHeader}>
+            <span>Shipments</span>
+          </div>
+          <div className={styles.statConBody}>
+            <span>{shipmentData.length}</span>
           </div>
         </div>
       </div>
