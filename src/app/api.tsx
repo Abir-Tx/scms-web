2b1902b4223578cb0eb79f48cd8942d8c3589e44
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-12-24 16:37:54 +0600

    Remove the total count API
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- src/app/api.tsx
+++ src/app/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "//localhost:3000/logistics", // Your API base URL
+  baseURL: "//localhost:3000/logistics",
 });
 
 export default api;
