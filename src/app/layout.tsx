ce99bb21c8293de01762fe86f1fe851cefff88f5
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-12-26 19:03:19 +0600

    Refactor layout component to wrap children in a main tag
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
