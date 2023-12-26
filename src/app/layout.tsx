ebb883cc8b51d41dd1e579e68e64ad79bf5b6f18
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-12-26 18:01:35 +0600

    Addition of authentication logic to different routes (#2)
    * Refactor input fields in DriverSearch component
    
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
    
    * Add authentication check for protected routes
    
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
    
    * Add "/lm/drivers/search" to protected routes
    
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,44 @@
+"use client";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+/* export const metadata: Metadata = {
   title: "SCMS Web",
   description:
     "Supply Chain Management System Frontend By Mushfiqur Rahman Abir. Make your supply management easy with SCMS.",
-};
+}; */
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const storedEmail = localStorage.getItem("email");
+    const isLoggedIn = !!storedEmail;
+
+    const protectedRoutes = [
+      "/lm/dashboard",
+      "/lm/shipments",
+      "/lm/transports",
+      "/lm/drivers",
+      "/lm/drivers/search",
+    ];
+
+    if (protectedRoutes.includes(pathname) && !isLoggedIn) {
+      router.push("/login");
+    }
+  }, [pathname, router]);
   return (
     <html lang="en">
       <body className={inter.className}>
